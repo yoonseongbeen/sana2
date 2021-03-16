6ae6ab17fdcf5827848a838c4c53530879f93261
yoonseongbeen <80637027+yoonseongbeen@users.noreply.github.com>
2021-03-17 00:20:30 +0900

    Add files via upload

---
+++ run.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+token = ("ODIwNjI1ODI5MjM4Mjc2MDk2.YE35XA.t9ox4_IrTTcnRPNz5ssDGEG3gKo")
+
+bot = commands.Bot(command_prefix = '!', help_command = None)
+
+
+
+
+@bot.event
+async def on_ready():
+    print(f'부팅 성공 : {bot.user.name}!')
+    game = discord.Game("조민준") 
+    await bot.change_presence(status = discord.Status.online, activity = game)
+
+
+@bot.command()
+async def 도움말(ctx):
+    embed = discord.Embed(title = "도움말", description = "**!가입**\n서비스에 가입할 수 있습니다.\n\n**!탈퇴**\n서비스에서 탈퇴할 수 있습니다.\n\n", color = 0xffc0cb)
+    embed.set_footer(text = f"{ctx.message.author.name} | 윤성빈#6115", icon_url = ctx.message.author.avatar_url)
+    await ctx.send(embed = embed)
+
+
+bot.run(token)
+
+
+
+
