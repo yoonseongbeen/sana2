7ad5129a2404ca20db95b5e79ca5cc4159aae221
yoonseongbeen <80637027+yoonseongbeen@users.noreply.github.com>
2021-03-28 13:22:59 +0900

    Update run.py

--- run.py
+++ run.py
@@ -20,6 +20,10 @@ async def 도움말(ctx):
     embed = discord.Embed(title = "도움말", description = "**!가입**\n서비스에 가입할 수 있습니다.\n\n**!탈퇴**\n서비스에서 탈퇴할 수 있습니다.\n\n", color = 0xffc0cb)
     embed.set_footer(text = f"{ctx.message.author.name} | 윤성빈#6115", icon_url = ctx.message.author.avatar_url)
     await ctx.send(embed = embed)
+    
+    @bot.command()
+async def 가입(ctx):
+    signup(ctx.author.name, ctx.author.id)
 
 
 bot.run(token)
